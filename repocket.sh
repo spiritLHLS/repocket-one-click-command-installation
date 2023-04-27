2b0f9328b636da0811a7432745589b4e2c227f06
SuperYuIuo <39399442+SuperYuIuo@users.noreply.github.com>
2023-04-27 21:04:35 +0800

    Update repocket.sh

--- repocket.sh
+++ repocket.sh
@@ -2,6 +2,16 @@
 #FROM https://github.com/spiritLHLS/repocket-one-click-command-installation
 
 
+utf8_locale=$(locale -a 2>/dev/null | grep -i -m 1 -E "UTF-8|utf8")
+if [[ -z "$utf8_locale" ]]; then
+  echo "No UTF-8 locale found"
+else
+  export LC_ALL="$utf8_locale"
+  export LANG="$utf8_locale"
+  export LANGUAGE="$utf8_locale"
+  echo "Locale set to $utf8_locale"
+fi
+
 # 定义容器名
 NAME='repocket'
 
